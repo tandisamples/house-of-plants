cca94e5e0d7af6ae82b4bdb9b925863b9a027397
Tandi Samples <tandisamples@gmail.com>
2020-01-30 01:11:41 +0000

    add controllers

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,16 +1,35 @@
 class OrderController < ApplicationController
   
   #RESTful routes
+    # get, post, patch, delete  = http verb 
   
   # Create 
   
+    # New 
+    # make a post request to '/orders/new'
+    
+    # Create 
     # make a post request to '/orders'
   
   #READ 
      # Index 
      # make a get request to /'orders'
     
+    get '/orders' do
+      erb :'orders/index'
+    end
+    
     # Show 
     # make a get request to '/orders/:id'
+    
+  # UPDATE 
+      # Edit 
+      # make a request to '/orders/:id/edit'
+      
+      # Update
+      # make a patch request to /orders/:id'
+      
+  # DESTROY
+      # make a delete request to '/orders/:id'
   
 end
